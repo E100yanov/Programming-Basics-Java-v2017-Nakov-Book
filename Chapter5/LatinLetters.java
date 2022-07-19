c4363505a551fa5f43ab78a7fc1557aeb7064a23
E100yanov <108828419+E100yanov@users.noreply.github.com>
2022-07-19 13:17:55 +0300

    Add files via upload

---
+++ Chapter5/LatinLetters.java
@@ -0,0 +1,10 @@
+package Chapter5;
+
+public class LatinLetters {
+    public static void main(String[] args) {
+
+        for(int i = 'a'; i <= 'z'; i++){
+            System.out.println((char)i);
+        }
+    }
+}

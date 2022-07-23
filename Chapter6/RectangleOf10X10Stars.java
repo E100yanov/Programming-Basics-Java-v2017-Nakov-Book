70826b8df67962fa29a2c09edf1c0c758e780841
E100yanov <108828419+E100yanov@users.noreply.github.com>
2022-07-23 11:37:18 +0300

    Add files via upload

---
+++ Chapter6/RectangleOf10X10Stars.java
@@ -0,0 +1,13 @@
+package Chapter6;
+public class RectangleOf10X10Stars {
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 10; i++){
+
+            for(int j = 1; j <= 10; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

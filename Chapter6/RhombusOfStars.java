adf8a3e47db745a69fe31e637859c6c31e7bc2f4
E100yanov <108828419+E100yanov@users.noreply.github.com>
2022-07-24 08:34:13 +0300

    Add files via upload

---
+++ Chapter6/RhombusOfStars.java
@@ -0,0 +1,32 @@
+package Chapter6;
+
+import java.util.Scanner;
+
+public class RhombusOfStars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for(int row = 1; row <= n - 1; row++){
+            for(int col = 1; col <= n - row; col++){
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            for(int col = 1; col < row; col++){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+
+        for(int row = n; row >= 1; row--){
+            for(int col = 1; col <= n - row; col++){
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            for(int col = 1; col < row; col++){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
+}
